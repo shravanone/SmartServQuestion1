5dd482004fbe80f77852e7bc473c7510282b8727
shravanone <78489777+shravanone@users.noreply.github.com>
2021-12-25 22:00:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,8 @@ fetch("https://s3.amazonaws.com/open-to-cors/assignment.json")
     .then(res=>res.json())
     .then(data=>{
         data = data.products
-        console.log(data);
-        console.log(typeof(data));
             var temp = "";
-            
             for (product in data){
-                console.log(data[product])
                 temp += "<tr>"
                 temp += "<td>"+data[product].subcategory+"</td>"
                 temp += "<td>"+data[product].title+"</td>"
